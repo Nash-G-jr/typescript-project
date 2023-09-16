202c4a675561b628a4dacfa8421ab826ccd7fc06
Luka Khergiani <luka.khergiani.99@mail.ru>
2023-09-16 23:51:05 +0400

    Hooks, Api, Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useProducts } from "./hooks/products";
 import { Product } from "./components/Product";
+import { Loader } from "./components/Loader";
+import { ErrorMessage } from "./components/ErrorMessage";
 
 function App() {
+  const { products, error, loading } = useProducts();
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product />
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
     </div>
   );
 }
